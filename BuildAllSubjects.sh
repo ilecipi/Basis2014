6eb743e2ac88719c395a37006f363a4856d8a7c7
pennatil <pennatil@student.ethz.ch>
2014-06-19 17:10:49 +0200

    Continuato PP, aggiunto script, altro
    - Continuato riassunto PP
    - Aggiunto uno script per eseguire il render dei file in automatico
    - Generato nuovi file PDF
    - Spostato le immagini in una cartella Figures @cisco94 !!
    - Creato piccolo readme per generare i file

---
+++ BuildAllSubjects.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#Render Analysis 
+cd "Analysis I&II/Riassunto (Formelsammlung)"
+
+for i in {1..3}
+do
+   pdflatex --interaction nonstopmode AnalysisZusammenfassung.tex
+done
+rm -f *.out *.log *.aux *.toc
+cd ../../
+
+
+#Render DigTech
+cd "Digitaltechnik/RiassuntoDT"
+for i in {1..3}
+do
+   pdflatex --interaction nonstopmode RiassuntoDT.tex
+done
+rm -f *.out *.log *.aux *.toc
+cd ../../
+
+
+#Render DiskMat
+cd "Diskrete Mathematik/Riassunto"
+for i in {1..3}
+do
+   pdflatex --interaction nonstopmode RiassuntoDiskMat.tex
+done
+rm -f *.out *.log *.aux *.toc
+cd ../../
+
+
+#Render DnA
+cd "DnA/Riassunto"
+for i in {1..3}
+do
+   pdflatex --interaction nonstopmode riassunto_DnA.tex
+done
+rm -f *.out *.log *.aux *.toc
+cd ../../
+
+#Render PP
+cd "Parallele Programmierung/Riassunto"
+for i in {1..3}
+do
+   pdflatex --interaction nonstopmode RiassuntoPP.tex
+done
+rm -f *.out *.log *.aux *.toc
+cd ../../
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 Basis2014
 =========
+Per eseguire il render dei file c'è un file chiamato <code>BuildAllSubjects.sh</code>, che funziona solo su Lunix e Mac. In questo modo è tutto automatico. Per eseguire il render:
+
+1. <code>cd path/to/Basis2014/folder/</code> (e.g. <code>cd /Users/Waldo/Desktop/Basis2014/</code>)
+2. Se è la prima volta che si fa girare lo script, scrivere <code>chmod 777 BuildAllSubjects.sh </code>
+3. Scrivere <code>./nameOfScript.sh</code> e premere enter. Lo script esegue i comandi e elimina i file inutili.
+4. In ogni cartella ci sara il riassunto appena generato
